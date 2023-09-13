29bfa50bccae08f3711b043f570f14f4df78ea7e
Muhammad Najeeb <yusnaj21@gmail.com>
2023-09-13 13:55:50 +0100

    BitForce Age Calculator project

---
+++ main.js
@@ -0,0 +1,35 @@
+function calculate() {
+  // Get input values
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var operator = document.getElementById("operator").value;
+  var resultElement = document.getElementById("result");
+
+  // Check if inputs are valid numbers
+  if (isNaN(num1) || isNaN(num2)) {
+    resultElement.innerText = "Please enter valid numbers.";
+    return;
+  }
+
+  // Perform calculation based on operator
+  switch (operator) {
+    case "+":
+      resultElement.innerText = num1 + num2;
+      break;
+    case "-":
+      resultElement.innerText = num1 - num2;
+      break;
+    case "*":
+      resultElement.innerText = num1 * num2;
+      break;
+    case "%":
+      resultElement.innerText = num1 % num2;
+      break;
+    case "/":
+      resultElement.innerText = num1 / num2;
+      break;
+    default:
+      resultElement.innerText =
+        "Invalid operator. Please use +, -, *, %, or /.";
+  }
+}
